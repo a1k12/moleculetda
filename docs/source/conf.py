5b309dcb825c8305fedbfd08c37057495c4cbf70
a1k12 <a1k2112@gmail.com>
2022-08-22 13:24:11 -0700

    Bump version: 0.2.0 → 0.2.1-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = f"{date.today().year}, Aditi Krishnapriyan "
 author = "Aditi Krishnapriyan "
 
 # The full version, including alpha/beta/rc tags.
-release = "0.2.0"
+release = "0.2.1-dev"
 
 # The short X.Y version.
 parsed_version = re.match(
